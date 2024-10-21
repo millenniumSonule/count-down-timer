890d2ec70cd27ab7d2af15c7ff9c3509011d78f0
millenniumSonule <anshumansonuley@gmail.com>
2024-10-21 11:04:51 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 const App = () => {
+
+  const [hour,setHour] = useState();
+  const [minutes,setMinutes] = useState();
+  const [seconds,setSeconds] = useState();
+
+  const handleHourInput = (e) =>{
+    // setHour(e.target.value);
+    const value = e.target.value;
+
+    if( value === '' || (Number(value)<100)) {
+      setHour(value);
+    }else {
+      setHour('')
+      alert('Hour should be less than 100');
+    }
+
+  }
+
+  const handleMinInput = (e) => {
+      const value = e.target.value;
+
+
+      if(value=== '' || value < 60) {
+        setMinutes(value)
+      }else {
+        setMinutes('')
+        alert('Minutes should be less than 60');
+      }
+
+  }
+
+  const handleSecondsInput = (e) =>{
+    // setHour(e.target.value);
+    const value = e.target.value;
+
+    if( value === '' || (Number(value)<60)) {
+      setSeconds(value);
+    }else {
+      setSeconds('')
+      alert('Seconds should be less than 60');
+    }
+
+  }
+
   return (
-    <div>App</div>
+    <div className='container'>
+      <span className='title'> <h1>Countdown Timer</h1></span>
+      <div className='timer-container d-flex'>
+            <input
+              type="number"
+              id="hour-box"
+              value={hour} // Controlled input, value is tied to state
+              onChange={handleHourInput} // Updates state on every change
+              placeholder="HH"
+              className='hour-box'
+            />
+
+            <input
+                type='number'
+                id='min-box'
+                value={minutes}
+                onChange={handleMinInput}
+                placeholder='MM'
+                className='min-box'
+            />
+
+            <input
+                type='number'
+                id='sec-box'
+                value={seconds}
+                onChange={handleSecondsInput}
+                placeholder='SS'
+                className='sec-box'
+            />
+
+
+      </div>
+    </div>
   )
 }
 
