e50aeb83d52721764293654501b65cf69f0684ff
millenniumSonule <anshumansonuley@gmail.com>
2024-10-26 22:34:26 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css'
 
 const App = () => {
 
-  const [hour,setHour] = useState();
-  const [minutes,setMinutes] = useState();
-  const [seconds,setSeconds] = useState();
+  const [hour, setHour] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
 
   const [isTimerStarted, setIsTimerStarted] = useState(false);
 
-  const handleHourInput = (e) =>{
+  const handleHourInput = (e) => {
     // setHour(e.target.value);
     const value = e.target.value;
 
-    if( value === '' || (Number(value)<100)) {
+    if (value === '' || (Number(value) < 100)) {
       setHour(value);
-    }else {
+    } else {
       setHour('')
       alert('Hour should be less than 100');
     }
@@ -23,25 +23,25 @@ const App = () => {
   }
 
   const handleMinInput = (e) => {
-      const value = e.target.value;
+    const value = e.target.value;
 
 
-      if(value=== '' || value < 60) {
-        setMinutes(value)
-      }else {
-        setMinutes('')
-        alert('Minutes should be less than 60');
-      }
+    if (value === '' || value < 60) {
+      setMinutes(value)
+    } else {
+      setMinutes('')
+      alert('Minutes should be less than 60');
+    }
 
   }
 
-  const handleSecondsInput = (e) =>{
+  const handleSecondsInput = (e) => {
     // setHour(e.target.value);
     const value = e.target.value;
 
-    if( value === '' || (Number(value)<60)) {
+    if (value === '' || (Number(value) < 60)) {
       setSeconds(value);
-    }else {
+    } else {
       setSeconds('')
       alert('Seconds should be less than 60');
     }
@@ -49,65 +49,117 @@ const App = () => {
 
 
   }
+  let intervalId;
+  const pauseButton = () => {
+    setIsTimerStarted(false);
+    if(!isTimerStarted) {
+      clearInterval(intervalId);
+    }
+  }
 
   const handleStartButton = () => {
-      const hourINSeconds =  Number(hour) * 60 * 60 || 0;
-      const minIndSeconds =  Number(minutes) * 60 || 0;
-      const totalTime = hourINSeconds + minIndSeconds + (Number(seconds) || 0);
+    const hourINSeconds = Number(hour) * 60 * 60 || 0;
+    const minIndSeconds = Number(minutes) * 60 || 0;
+    const totalTime = hourINSeconds + minIndSeconds + (Number(seconds) || 0);
+
+    let seconds_to_display = totalTime%60;
+    let min_to_display =Math.floor((totalTime/60)%60);
+    let hours_to_display = Math.floor(totalTime / (60 * 60)); 
+
+    setIsTimerStarted(true);
+
+    let remainingTime = totalTime;
+
     
-      setIsTimerStarted(true);
+     intervalId = setInterval(() => {
+     
+
       
-      if(isTimerStarted){
-       
-        setInterval(()=>{
-          
-          alert(totalTime)
-          console.log('hours: ' + (totalTime / 3600));
-          console.log('hours: ' + (totalTime / 3600));
-          console.log('seconds' + (totalTime))
-        },totalTime)
+      if(seconds_to_display===0) {
+        if(remainingTime>59){
+          seconds_to_display=59;
+          if (min_to_display > 0) {
+            min_to_display--; // Decrease minutes when seconds reset
+          } else if (hours_to_display > 0) {
+            min_to_display = 59; // Reset minutes to 59 if we still have hours left
+            hours_to_display--; // Decrease the hour
+          }
+        }else{
+          seconds_to_display=remainingTime;
+        }
       }
-    
+      
+      if (remainingTime > 0) {
+        remainingTime--;
+        setSeconds(seconds_to_display);
+        seconds_to_display--;
+        
+        
+       
+        if (seconds_to_display === 0 && min_to_display > 0) {
+          // min_to_display--;
+        }
+        setMinutes(min_to_display%60);
+        setHour(hours_to_display);
+        console.log(remainingTime);
+      } else {
+        clearInterval(intervalId);
+        setIsTimerStarted(false);
+      }
+    }, 1000);
+
+
   }
 
   return (
     <div className='container'>
       <span className='title'> <h1>Countdown Timer</h1></span>
-      <div className='timer-container d-flex'>
-            <input
-              type="number"
-              id="hour-box"
-              value={hour} // Controlled input, value is tied to state
-              onChange={handleHourInput} // Updates state on every change
-              placeholder="HH"
-              className='hour-box'
-            />
+        {isTimerStarted? 
+        
+        <div className='timer-container'>
 
-            <input
-                type='number'
-                id='min-box'
-                value={minutes}
-                onChange={handleMinInput}
-                placeholder='MM'
-                className='min-box'
-            />
+          <span>{hour} : {minutes} : {seconds}</span>
 
-            <input
-                type='number'
-                id='sec-box'
-                value={seconds}
-                onChange={handleSecondsInput}
-                placeholder='SS'
-                className='sec-box'
-            />
-
-            <button className='start-button' onClick={handleStartButton}>
-              
-              Start
-            </button>
-            
-
-      </div>
+          <button className='start-button' onClick={pauseButton}>
+  
+  Pause
+</button>
+        </div> :
+          <div className='timer-container d-flex'>
+          <input
+            type="number"
+            id="hour-box"
+            value={hour} // Controlled input, value is tied to state
+            onChange={handleHourInput} // Updates state on every change
+            placeholder="HH"
+            className='hour-box'
+          />
+  
+          <input
+            type='number'
+            id='min-box'
+            value={minutes}
+            onChange={handleMinInput}
+            placeholder='MM'
+            className='min-box'
+          />
+  
+          <input
+            type='number'
+            id='sec-box'
+            value={seconds}
+            onChange={handleSecondsInput}
+            placeholder='SS'
+            className='sec-box'
+          />
+  
+          <button className='start-button' onClick={handleStartButton}>
+  
+            Start
+          </button>
+  
+  
+        </div>}
     </div>
   )
 }
